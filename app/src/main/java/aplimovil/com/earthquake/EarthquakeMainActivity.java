6239b8d9af92b404d0c043e9986cc5f761947060
aplimovil <fomarti@unicauca.edu.co>
2019-03-27 10:48:08 -0500

    Initial setup

---
+++ app/src/main/java/aplimovil/com/earthquake/EarthquakeMainActivity.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * Applications development for mobile devices
+ * -------------------------------------------
+ *
+ * Earthquake example implements an an app that will use a feed of earthquake
+ * data from the United States Geological Survey (USGS) to display a list
+ * (and eventually a map) of recent earthquakes.
+ *
+ * @author Francisco Martinez
+ *
+ * This example is based on the source code from the book
+ * of Reto Meier and Ian Lake - Professional Android
+ * licensed under the Apache License, Version 2.0 (the "License"):
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ */
+
+
+package aplimovil.com.earthquake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EarthquakeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
